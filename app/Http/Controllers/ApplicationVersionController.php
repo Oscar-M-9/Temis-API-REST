a8d63022c13d3f8940312a81c827f9ada6c91892
Oscar-M-9 <oc6781343@gmail.com>
2024-03-06 12:56:58 -0500

    fix: models application-version

--- app/Http/Controllers/ApplicationVersionController.php
+++ app/Http/Controllers/ApplicationVersionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\models\ApplicationVersion;
+use App\Models\ApplicationVersion;
 
 class ApplicationVersionController extends Controller
 {
